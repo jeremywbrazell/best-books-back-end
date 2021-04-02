5fe31251ae885515be8fd714b939bbe6096b7b3b
TrunkOfUkuleles <60909020+TrunkOfUkuleles@users.noreply.github.com>
2021-04-02 09:31:43 -0700

    l

--- server.js
+++ server.js
@@ -7,83 +7,63 @@ const User = require('./modules/User');
 const app = express();
 app.use(cors());
 app.use(express.json())
-// import Booking from './test modules'
-
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb://HVVZOZJW:dcd6c5f5-2fde-48be-b010-2c3429ee2f5f@cluster0.ktbir.mongodb.net";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
-// const connectionString = "mongodb+srv://cluster0.ktbir.mongodb.net/myFirstDatabase?retryWrites=true&w=majority/"
 
 const mongoose = require('mongoose');
 mongoose.connect(process.env.ATLAS_CONN, { 
     dbName: "BestBooksDev",
-    user: process.env.JCLOUD_ATLUS_USER,
+    dbCollection: "users",
+    user: process.env.JCLOUD_ATLAS_USER,
     pass: "Postman1234",
     useNewUrlParser: true, 
     useUnifiedTopology: true,
-    useFindAndModify: false })
+    // useFindAndModify: false 
+})
 .then(()=>{
-    
-    console.log(`Mongo connected to atlus?!`)})
+console.log(`Mongo connected to atlus?!`)})
 
 const db = mongoose.connection;
-
 db.on('error', console.error.bind(console, 'connection error:'));
-
 db.once('open', function () {
-    console.log('connected in DB once!');
-    
-});
+    console.log('connected in DB once!')
+}).catch(error => console.log("DB Connection down" , error));
+
 async function getBooks(request, response) {
     const name = request.query.email;
-    console.log('getting: ', name)
+console.log('getting: ', name)
     await User.find({email: name.email}, function (err, books){
         if (err)return console.error(err);
-        console.log('name: ', name);
+ console.log('name: ', name);
         response.status(200).send(books);
-    })
-    .catch(error => {console.error(error)});
+    }).catch(error => {console.error(error)});
 }
 
-// async function booky(req,res) {
-//     const useremail = req.query.email;
-//     console.log(useremail)
-//     await User.find({email: useremail.email}, function(err,item){
-//         if(err) return console.error(err);
-//         res.status(200).send(item)
-//     })
-// }
-
 async function posty(req, res){
-    const data = req.body.book;
-    const own = req.body.email;
+    const data = req.query.book;
+    const own = req.query.email;
     await User.find({own}, (err, itm)=>{
         if(err) return console.error(err);
         itm.books.push({name: data.name, description: book.description, status: book.status })
         itm.save()
         res.status(200).send(itm.books)
-        
     }) 
 }
 
-async function putter (req, res){
-    const me = req.query.name;
-    const changer = req.query.index;
-    await User.find({me}, (err, change)=>)
+// async function putter (req, res){
+//     const me = req.query.name;
+//     const changer = req.query.index;
+//     await User.find({me}, (err, change)=>{
+//         if(err) return console.error(err);
+//     }
+    
+//     )
 
-}
+// }
 
 async function killer(req,res){
     const data = parseInt(req.query.index);
     const me = req.query.email
 
-    await User.find({me}, (err, hit)=>{
+    await User.find({email: me}, (err, hit)=>{
         if(err) return console.error(err);
         const neew = hit.books.filter((thing, i)=>{
             i !== data
@@ -103,7 +83,7 @@ app.get('/', function (req, res){
 app.get('/books', getBooks);
 app.post('/books', posty);
 app.delete('/books', killer);
-app.put('/books', putty)
+// app.put('/books', putter)
 
 
 
